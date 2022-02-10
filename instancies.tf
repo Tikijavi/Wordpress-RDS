62591804b7033b5b5f853b3d4b06e15336a143d4
Tikijavi <74424481+Tikijavi@users.noreply.github.com>
2022-02-10 16:23:43 +0100

    Update instancies.tf

--- instancies.tf
+++ instancies.tf
@@ -6,15 +6,15 @@ resource "aws_key_pair" "keypair" {
 }
 
 # Instància EC2 amb la instal·lació del wordpress
-resource "aws_instance" "WordPress" {
+resource "aws_instance" "Wordpress" {
   depends_on = [aws_internet_gateway.public_internet_gw]
-  ami           = "ami-0732b62d310b80e97"
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.deployer.key_name
+  ami           = var.ami_ec2
+  instance_type = var.instance_type_ec2
+  key_name      = aws_key_pair.keypair.key_name
   subnet_id = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.SG_public_subnet.id]
   tags = {
-     Name = "WEB"
+     Name = "Wordpress"
   }
 
   user_data = <<EOF
@@ -36,7 +36,7 @@ resource "aws_instance" "WordPress" {
       EOF
 
  provisioner "local-exec" {
-  command = "echo ${aws_instance.WordPress.public_ip} > publicIP.txt"
+  command = "echo ${aws_instance.Wordpress.public_ip} > publicIP.txt"
  }
 
 }
@@ -44,15 +44,15 @@ resource "aws_instance" "WordPress" {
 # Llençant la base de dades RDS
 resource "aws_db_instance" "DataBase" {
   allocated_storage    = 20
-  max_allocated_storage = 100
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7.22"
-  instance_class       = "db.t2.micro"
-  name                 = "javieselmillor"
-  username             = "javier"
-  password             = "1q2w3e4R"
-  parameter_group_name = "default.mysql5.7"
+#  max_allocated_storage = 100
+  storage_type         = var.storage_type_db
+  engine               = var.engine_type_db
+  engine_version       = var.engine_version_db
+  instance_class       = var.instance_type_db
+  name                 = var.name_db
+  username             = var.username_db
+  password             = var.password_db
+  parameter_group_name = var.group_db
   publicly_accessible = false
   db_subnet_group_name = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids = [aws_security_group.SG_private_subnet_.id]
