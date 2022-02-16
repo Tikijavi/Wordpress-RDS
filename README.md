e87dec29a4a346f169eca1df74739d9925bb1873
Tikijavi <procod666wtf@gmail.com>
2022-02-16 14:02:09 +0000

    estaes

---
+++ README.md
@@ -0,0 +1,10 @@
+# Proyect-ASIX
+
+    Group members: Dennis Rayo, Javier Sánchez
+    Overview: Mail server in AWS with a RDS in Terraform
+    Goals: Make a Terraform script that launches a EC2 instance with the mail server and a RDS to use as DB
+    Specifications: 
+    Issues / risks: The main issue is if we will be able to do a Terraform script.
+    Availability (temps disponible per dev): classtime
+    Environment (technologies): AWS, Terraform, Nginx, RDS (MySQL), Amazon Linux 2, EC2, VPC, Dovecot, Postfix, ISPConfig
+    Scheduling (deliverables): 28/01/22 terraform script (infrastructure only), 11/02/22 script service installation, 25/02/22 mini demo, 04/03/22 1.0
---
+++ instancies.tf
@@ -0,0 +1,76 @@
+# Claus de la instancia
+resource "aws_key_pair" "keypair" {
+    key_name    = var.key_pair
+    #public_key  = "mykey.pub"
+    public_key  = "${file("mykey.pub")}"
+}
+
+# Instància EC2 amb la instal·lació del wordpress
+resource "aws_instance" "Wordpress" {
+  depends_on = [aws_internet_gateway.public_internet_gw]
+  ami           = var.ami_ec2
+  instance_type = var.instance_type_ec2
+  key_name      = aws_key_pair.keypair.key_name
+  subnet_id = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.SG_public_subnet.id]
+  tags = {
+     Name = "Wordpress"
+  }
+
+  user_data = <<EOF
+         #! /bin/bash
+             sudo yum install httpd php php-mysql -y -q
+             sudo cd /var/www/html
+             echo "Welcome" > hi.html
+             sudo wget https://wordpress.org/wordpress-5.1.1.tar.gz
+             sudo tar -xzf wordpress-5.1.1.tar.gz
+             sudo cp -r wordpress/* /var/www/html/
+             sudo rm -rf wordpress
+             sudo rm -rf wordpress-5.1.1.tar.gz
+             sudo chmod -R 755 wp-content
+             sudo chown -R apache:apache wp-content
+             sudo wget https://s3.amazonaws.com/bucketforwordpresslab-donotdelete/htaccess.txt
+             sudo mv htaccess.txt .htaccess
+             sudo systemctl start httpd
+             sudo systemctl enable httpd
+      EOF
+
+ provisioner "local-exec" {
+  command = "echo ${aws_instance.Wordpress.public_ip} > publicIP.txt"
+ }
+
+ provisioner "file" {
+  content     = "wp-config.php" 
+  destination = "/var/www/html/wordpress/wp-config.php"
+ }
+
+ provisioner "remote-exec" {
+  inline = [
+   "echo define( 'DB_HOST', '${aws_db_instance.DataBase.endpoint}'); > /var/www/html/wordpress/wp-config.php ",
+   ]
+ }
+}
+
+# Llençant la base de dades RDS
+resource "aws_db_instance" "DataBase" {
+  allocated_storage    = 20
+
+#  max_allocated_storage = 100
+  storage_type         = var.storage_type_db
+  engine               = var.engine_type_db
+  engine_version       = var.engine_version_db
+  instance_class       = var.instance_type_db
+  name                 = var.name_db
+  username             = var.username_db
+  password             = var.password_db
+  parameter_group_name = var.group_db
+  publicly_accessible = false
+  db_subnet_group_name = aws_db_subnet_group.db_subnet.name
+  vpc_security_group_ids = [aws_security_group.SG_private_subnet_.id]
+  skip_final_snapshot = true
+
+provisioner "local-exec" {
+  command = "echo ${aws_db_instance.DataBase.endpoint} > DB_host.txt"
+    }
+
+}
