a69427888785758eeb1f3cf11b531422edaaadb8
Joel <57760071+JoelCasas11@users.noreply.github.com>
2022-03-16 16:32:13 +0100

    Update instancies.tf

--- instancies.tf
+++ instancies.tf
@@ -79,7 +79,7 @@ resource "aws_s3_bucket" "b" {
 
 #Backups
 source backup.sh
-sudo mysqldump -u javier -p 1q2w3e4R --all-databases | gzip > mysqldb_`date +%F`.sql.gz
+sudo mysqldump -u javier -p 1q2w3e4R --all-databases | gzip > mysqldb_ date +%F .sql.gz
 sudo crontab -e
 30 23 * * * backup.sh
 
